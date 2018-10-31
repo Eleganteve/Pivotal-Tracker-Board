727b84588646f5ac29f7b2ed9f5bb2d75b1b8551
phoebekaranja <phoebemurish@gmail.com>
2018-11-01 02:43:36 +0300

    adding some changes

--- app/_init_.py
+++ app/_init_.py
@@ -1,49 +1,52 @@
 from flask import Flask
+from config import config_options,Config
 from flask_bootstrap import Bootstrap
-from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
-from flask_moment import Moment
 from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
-simplemde = SimpleMDE()
+simple = SimpleMDE()
+
 
 def create_app(config_name):
     app = Flask(__name__)
 
-    '''
-    creating the app configurations
-    '''
-     #creating the app configurations
+    # Creating the app configuration
     app.config.from_object(config_options[config_name])
-    login_manager.session_protection = 'strong'
-    login_manager.login_view = 'auth.login'
+    app.config.from_object(Config)
 
-     #initializing the flask extensions
+
+
+
+    # initializing flask extensions
+
     bootstrap.init_app(app)
     db.init_app(app)
+
+    simple.init_app(app)
     login_manager.init_app(app)
-    mail.init_app(app)
-    simplemde.init_app(app)
 
-
-    # configure UploadSet
-    configure_uploads(app,photos)
-
-
-    #Registering the bluprint
+    # Registering the blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    mail.init_app(app)
+
 
 
     return app
